a87d0cef0bb2895f19c6d0d1f3f15bb5db9e0975
Pedro Soares <pedrinhocef@gmail.com>
2018-06-10 19:36:49 -0300

    Criando menu para realizar busca por data das provas fim do curso

--- app/src/main/java/br/com/alura/roomapplication/database/dao/ProvaDAO.java
+++ app/src/main/java/br/com/alura/roomapplication/database/dao/ProvaDAO.java
@@ -6,6 +6,7 @@ import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
+import java.util.Calendar;
 import java.util.List;
 
 import br.com.alura.roomapplication.models.Prova;
@@ -16,8 +17,11 @@ public interface ProvaDAO {
     @Insert
     void insere(Prova prova);
 
-    @Query("select * from prova")
+    @Query("select * from Prova")
     List<Prova> buscaProva();
+
+    @Query("select * from Prova where dataRealizacao between :inicio and :fim")
+    List<Prova> buscaPeloPeriodo(Calendar inicio, Calendar fim);
 
     @Update
     void altera(Prova prova);
--- app/src/main/java/br/com/alura/roomapplication/fragment/ListaProvasFragment.java
+++ app/src/main/java/br/com/alura/roomapplication/fragment/ListaProvasFragment.java
@@ -1,36 +1,99 @@
 package br.com.alura.roomapplication.fragment;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import java.util.Calendar;
 import java.util.List;
 
 import br.com.alura.roomapplication.R;
 import br.com.alura.roomapplication.database.GeradorDeBancoDeDados;
+import br.com.alura.roomapplication.database.conversor.ConversorDeData;
 import br.com.alura.roomapplication.database.dao.ProvaDAO;
-import br.com.alura.roomapplication.delegate.AlunoDelegate;
 import br.com.alura.roomapplication.delegate.ProvasDelegate;
-import br.com.alura.roomapplication.models.Aluno;
 import br.com.alura.roomapplication.models.Prova;
 
 public class ListaProvasFragment extends Fragment {
 
     private ProvasDelegate delegate;
     private FloatingActionButton botaoAdd;
+    private ListView listagem;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         delegate = (ProvasDelegate) getActivity();
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.lista_provas_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_lista_prova_calendario) {
+            final Context contexto = getContext();
+
+            LinearLayout linearLayout = new LinearLayout(contexto);
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+            final EditText campoInicio = new EditText(contexto);
+            campoInicio.setHint("Inicio");
+            campoInicio.setInputType(InputType.TYPE_DATETIME_VARIATION_DATE);
+
+            final EditText campoFim = new EditText(contexto);
+            campoFim.setHint("Fim");
+            campoFim.setInputType(InputType.TYPE_DATETIME_VARIATION_DATE);
+
+            linearLayout.addView(campoInicio);
+            linearLayout.addView(campoFim);
+
+            new AlertDialog.Builder(contexto)
+                    .setView(linearLayout)
+                    .setMessage("Digite as datas para busca")
+                    .setPositiveButton("Buscar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String dataInicioString = campoInicio.getText().toString();
+                            String dataFimString = campoFim.getText().toString();
+
+                            Calendar dataInicio = ConversorDeData.converte(dataInicioString);
+                            Calendar dataFim = ConversorDeData.converte(dataFimString);
+
+                            ProvaDAO provaDAO = new GeradorDeBancoDeDados().gera(contexto).getProvaDAO();
+                            List<Prova> listProva = provaDAO.buscaPeloPeriodo(dataInicio, dataFim);
+
+                            configuraAdapter(contexto, listProva);
+
+                        }
+                    })
+                    .setNegativeButton("Cancelar", null)
+                    .show();
+        }
+        return true;
     }
 
     @Nullable
@@ -38,29 +101,37 @@ public class ListaProvasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lista, container, false);
 
-        final ListView lista = view.findViewById(R.id.fragment_lista);
+        configuraListagem(view);
 
-        final ProvaDAO provaDAO = new GeradorDeBancoDeDados().gera(getContext()).getProvaDAO();
+        configuraFAB(view);
+
+        return view;
+
+    }
+
+    private void configuraListagem(View view) {
+        listagem = view.findViewById(R.id.fragment_lista);
+
+        Context contexto = getContext();
+        final ProvaDAO provaDAO = new GeradorDeBancoDeDados().gera(contexto).getProvaDAO();
         List<Prova> provas = provaDAO.buscaProva();
 
-        final ArrayAdapter<Prova> adapter =
-                new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, provas);
-        lista.setAdapter(adapter);
+        final ArrayAdapter<Prova> adapter = configuraAdapter(contexto, provas);
 
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listagem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Prova prova = (Prova) lista.getItemAtPosition(position);
+                Prova prova = (Prova) listagem.getItemAtPosition(position);
 
                 delegate.lidaComProvaSelecionado(prova);
             }
         });
 
-        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        listagem.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                final Prova prova = (Prova) lista.getItemAtPosition(position);
+                final Prova prova = (Prova) listagem.getItemAtPosition(position);
 
                 String mensagem = "Excluir prova " + prova.getMateria() + " ?";
                 Snackbar.make(botaoAdd, mensagem ,Snackbar.LENGTH_LONG)
@@ -76,11 +147,13 @@ public class ListaProvasFragment extends Fragment {
                 return true;
             }
         });
+    }
 
-        configuraFAB(view);
-
-        return view;
-
+    @NonNull
+    private ArrayAdapter<Prova> configuraAdapter(Context contexto, List<Prova> provas) {
+        final ArrayAdapter<Prova> adapter = new ArrayAdapter<>(contexto, android.R.layout.simple_list_item_1, provas);
+        listagem.setAdapter(adapter);
+        return adapter;
     }
 
     private void configuraFAB(View view) {
